db36351e94ea4db69b6e4fa361949fdfbee9b38b
DAndrewA <Andrew.Martin@bourne-grammar.lincs.sch.uk>
2015-01-31 19:43:57 +0000

    Changed for python3.4 and added note to read me. Adding destruction of bodies.

--- 3BP.py
+++ 3BP.py
@@ -1,9 +1,9 @@
-from Tkinter import *
+from tkinter import *
 import math
 import time
 
 app = Tk()
-c = Canvas(app,height="500",width="500")
+c = Canvas(app,height="750",width="750")
 c.pack()
 
 # Defines the body object. It will store the position and velocity vector for the body, aswell as its mass and the ability to calculate specific values.
@@ -32,32 +32,17 @@ def calcDistance(pos1,pos2):
     distance = math.sqrt(distanceV[0]**2 + distanceV[1]**2)
     return distance
 
-# Claculates the position of the centre of gravity - COG
-def calcCOG(bodies):
-    # Sets the values for the COG's mass and x,y position
-    COGx = 0
-    COGy = 0
-    COGm = 0
-    # Iterates through bodies and adds a weighted average of their positions to get a mass accurate COG
-    for i in range(len(bodies)):
-        COGx += bodies[i].position[0]*bodies[i].mass
-        COGy += bodies[i].position[1]*bodies[i].mass
-        COGm += bodies[i].mass
-    # Divides the values by the overall mass to get the weighted average
-    COGx = COGx / COGm
-    COGy = COGy / COGm
-    # Returns: x,y,mass
-    return [COGx,COGy,COGm]
-
 # Instantiating the objects and setting their positions and velocities
-body1 = body([350,150],[0,0],100,0)
+body1 = body([350,150],[-10,-30],200,0)
 body2 = body([150,250],[30,10],100,1)
-body3 = body([350,350],[-20,-10],100,2)
+body3 = body([350,350],[-20,40],200,2)
+body4 = body([500,350],[0,0],2000,3)
 
 # Drawing the bodies onto the canvas
 c.create_oval(body1.position[0]-5,body1.position[1]-5,body1.position[0]+5,body1.position[1]+5,tag="body1Circle",fill="green")
 c.create_oval(body2.position[0]-5,body2.position[1]-5,body2.position[0]+5,body2.position[1]+5,tag="body2Circle",fill="blue")
 c.create_oval(body3.position[0]-5,body3.position[1]-5,body3.position[0]+5,body3.position[1]+5,tag="body3Circle",fill="red")
+c.create_oval(body4.position[0]-5,body4.position[1]-5,body4.position[0]+5,body4.position[1]+5,tag="body4Circle",fill="purple")
 
 #global lastFrame
 #lastFrame = time.time()
@@ -66,7 +51,7 @@ gravityConstant = 500
 # Makes the program run forever (or until it is closed)
 while True:
     # Updates the bodies variable with the current objects
-    bodies = [body1,body2,body3]
+    bodies = [body1,body2,body3,body4]
     # Gets the time frame to multiply by and sets the frame values
     deltaTime =  0.0005
     # Goes through all bodies
@@ -74,7 +59,7 @@ while True:
         # Calculates the accelaration for all of the bodies before moving them
         for x in bodies:
             if i.id != x.id:
-                COG = calcCOG([i,x])
+                COG = x.position
                 # Force = G * ((M1*M2)/distance^2)
                 F = gravityConstant*((i.mass*x.mass)/(calcDistance(COG,i.position)**2))
                 # Break down force into x and y components
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 3BodyProblem
-Solving a three body problem using physics, python and javascript
+Solving a three body problem using physics, python and - hopefully - javascript
+
+The python script is running in python 3.4 using TKinter
