15defd2f2f5ff9e86d2d5b34d359d2e661b716e6
DAndrewA <Andrew.Martin@bourne-grammar.lincs.sch.uk>
2015-02-23 18:29:11 +0000

    I give up for today. Javascript being awkward

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-var c = document.getElementById("canvas");
+var c = document.getElementById("Canvas");
 var ctx = c.getContext("2d");
 // ctx.fillStyle = "#(colour)"
 // ctx.beginPath();
@@ -8,21 +8,23 @@ var ctx = c.getContext("2d");
 var density = 2.5
 
 function body(startPos,startVel,mass,id,colour){
+    this.drawBody = function(colour){
+        // Calculate the radius of the body
+        volume = density*this.mass;
+        this.radius = Math.pow((volume/((4/3)*Math.PI)),1/3);
+
+        ctx.fillStyle = colour;
+        ctx.beginPath();
+        ctx.drawArc(this.position[0],this.position[1],this.radius,0,2*Math.PI);
+        ctx.stroke();
+    }
+
     this.position = startPos;
     this.velocity = startVel;
     this.mass = mass;
     this.id = id;
-    this.drawBody(colour);
-
-    this.drawBody  = function(colour){
-        // Calculate the radius of the body
-        volume = density*this.mass;
-        this.radius = pow((volume/((4/3)*Math.PI)),1/3);
-
-        ctx.fillStyle = colour;
-        ctx.beginPath(this.position[0],this.position[1],this.radius,0,2*Math.PI);
-        ctx.drawArc();
-    }
+    //this.drawBody(colour);
 }
 
 var test1 = new body([0,0],[0,0],500,1,"#FF00FF");
+test1.drawBody("#FF00FF")
